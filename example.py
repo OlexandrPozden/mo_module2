6a281cfcfebbf81b7490e80e30a6dc0f153a2a79
Alex <pozden.sasha@gmail.com>
2021-05-06 17:59:33 +0300

    gradient method

--- example.py
+++ example.py
@@ -1,15 +1,19 @@
 import mo_module2 as mm2
+from numpy import sqrt, array
 
 class Example(mm2.Gradient):
-    x0 = [0,0]
-
+    x0 = [-2,-2]
+    e = 0.000001
     @staticmethod
     def f(x1,x2):
-        pass
-    print(type(f))
+        return sqrt(x1**2+x2**2+1)+0.5*x1-0.5*x2
 
     @staticmethod
     def f_der(x1,x2):
-        pass
+        return array([
+            x1/sqrt((x1**2+x2**2+1)) + 0.5,
+            x2/sqrt((x1**2+x2**2+1)) - 0.5
+        ])
 
-Example.solve()
+res = Example.solve()
+print(res)
--- mo_module2/unconditional_min.py
+++ mo_module2/unconditional_min.py
@@ -1,22 +1,72 @@
 from inspect import signature
+from numpy.linalg import norm
+from numpy import array
 
 class Base:
     n = 0
+    e = 0.0001
+    x0 = [0,0]
+
     def __init__(self):
         pass
 
+    def f(self):
+        pass
+
+    def f_der(self):
+        pass
+
+    def _get_length_params(self):
+        sig = signature(self.f)
+        params = sig.parameters 
+        return len(params)
+
     def _prelength(foo):
         def dummie(self, *args, **kwargs):
-            sig = signature(self.f)
-            params = sig.parameters 
-            self.n = len(params)
+            self.n = self._get_length_params(self)
             foo(self, *args, **kwargs)
         return dummie
 
-    @classmethod
-    @_prelength
-    def solve(self,):
-        print(self.n)
-
 class Gradient(Base):
-    pass
+    
+    @classmethod
+    @Base._prelength
+    def solve(self, m=100):
+
+        # consts
+        x = self.x0
+        f = self.f
+        f_der = self.f_der
+        e = self.e
+
+        dfx = f_der(*x)
+        if norm(dfx)<e:
+            return x
+        
+        alpha = 1
+        
+        x = array(x)
+        x_ = x - alpha*dfx ## dfx = f'(x) = f'(xk) = f'(x0)
+        
+        while norm(f_der(*x_))>norm(dfx):
+            alpha/=2
+            x_ = x - alpha*dfx
+            
+        i = 0
+        while  norm(dfx)>e and i<m:
+            x = x_
+            dfx = f_der(*x)
+            
+            x_ = x - alpha*dfx
+            
+            while norm(f_der(*x_))>norm(dfx):
+                alpha/=2
+                x_ = x - alpha*dfx
+                
+            i += 1
+
+        print("iterations: ",i)
+        print(x_)
+        return x_
+
+    
